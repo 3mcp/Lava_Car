3d39b0fae7374ea84b307107862fb7e0bc0117e4
Murilo Carvalho Pereira <137966397+3mcp@users.noreply.github.com>
2024-08-04 23:44:18 -0300

    Add files via upload

---
+++ Moto.java
@@ -0,0 +1,11 @@
+public class Moto extends Veiculo{
+
+    public Moto(String ano, String modelo, String marca) {
+        super(ano, modelo, marca);
+    }
+
+    @Override
+    public void exibirInfo() {
+
+    }
+}
